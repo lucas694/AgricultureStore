0fdc35bfafa802f54c84c278cfde6e5ec2aaeb9b
lucas694 <lucasassuncao694@gmail.com>
2023-02-20 16:22:41 -0300

    a

--- src/Components/HeaderMobile.js
+++ src/Components/HeaderMobile.js
@@ -2,12 +2,22 @@ import "./HeaderMobile.css"
 import {NavLink} from "react-router-dom";
 import {FaBars} from "react-icons/fa";
 import {BsCart3} from "react-icons/bs";
+import {useState} from "react";
+import LogoHeader from "../Assets/LogoHeader.svg"
+
 
 const HeaderMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
   return(
     <div className={"HeaderMobileContainer"}>
       <div className={"HeaderMobileContent"}>
-        <ul>
+        <img src={LogoHeader} alt="LogoHeader" className={isOpen ? "hidden" : "LogoMobileHeader"}/>
+
+        <ul className={isOpen ? "flex" : "hidden"}>
           <NavLink to={"/"} className={({ isActive }) =>
             isActive ? "NavLinkMobileIsActive" :"LiNavLinkMobile" }>
             Home
@@ -33,7 +43,7 @@ const HeaderMobile = () => {
           <button className={"BtnMobileCart"}>
             <BsCart3 className={"CartMobileIcon"}/>
           </button>
-          <button >
+          <button onClick={toggle}>
             <FaBars className={"BtnMobileIcon"}/>
           </button>
         </div>
